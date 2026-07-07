db59c60da90619774fb1ced6f5c1320ac7b19814
W.A.Charith Jeewaka <wickramasinghajeewaka@gmail.com>
2026-07-07 08:38:37 +0530

    item model created

---
+++ src/models/Item.ts
@@ -0,0 +1,76 @@
+import { Document, Schema, model } from "mongoose";
+
+export interface IItem extends Document {
+  name: string;
+  category: string;
+  description?: string;
+  price: number;
+  stock: number;
+  image: string;
+  active: boolean;
+
+  averageRating: number;
+  reviewCount: number;
+}
+
+const itemSchema = new Schema<IItem>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      default: "",
+    },
+
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+
+    stock: {
+      type: Number,
+      required: true,
+      min: 0,
+      default: 0,
+    },
+
+    image: {
+      type: String,
+      required: true,
+    },
+
+    active: {
+      type: Boolean,
+      default: true,
+    },
+
+    averageRating: {
+      type: Number,
+      default: 0,
+      min: 0,
+      max: 5,
+    },
+
+    reviewCount: {
+      type: Number,
+      default: 0,
+      min: 0,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const ItemModel = model<IItem>("Item", itemSchema);
